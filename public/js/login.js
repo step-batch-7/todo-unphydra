0ea335201322e481b9aab318eaf5b6d7cd2b7878
rivu <58026024+unphydra@users.noreply.github.com>
2020-02-10 12:29:08 +0530

    added:connection between client and server established

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,8 @@
 const checkAndSend = function() {
   const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
+  post('/login', JSON.stringify({ username, password }), callAfterLogin);
+};
+const callAfterLogin = function(req) {
+  window.location.href = 'homePage.html';
 };
