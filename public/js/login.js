0a6e0b0e017ae36eac3b8439d42dc56bc54f93f3
rivu <58026024+unphydra@users.noreply.github.com>
2020-02-12 18:51:45 +0530

    added:set cookie and register a user

--- public/js/login.js
+++ public/js/login.js
@@ -16,7 +16,7 @@ const checkAndSend = function(type) {
   post(`/${type}`, JSON.stringify({ username, password }), callAfterLogin);
 };
 const callAfterLogin = function(req) {
-  window.location.href = 'homePage.html';
+  window.location.href = 'html/homePage.html';
 };
 
 window.onload = main;
